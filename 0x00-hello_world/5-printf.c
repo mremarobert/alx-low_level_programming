caab3c1d452d94f7de9644a0c284fc1603a0ba9c
robert.mrema <rkarionga@gmail.com>
2022-06-30 20:42:26 +0000

    C commands betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  * "with proper grammar, but the outcome is a piece of art,\n"
  * @\n: this prints a new line
  * Return: 1 if the number is positive. 0 otherwise
-*/
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
