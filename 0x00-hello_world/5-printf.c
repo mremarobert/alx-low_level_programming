fbdd184fe00816d0b17f5bc05437e8eab3738ca5
robert.mrema <rkarionga@gmail.com>
2022-06-30 20:40:20 +0000

    C commands betty style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
-
-int main(void) /* main function prints below text */
+/**
+ * main - this is the function or signature
+ * Description: Prints 
+ * "with proper grammar, but the outcome is a piece of art,\n"
+ * @\n: this prints a new line
+ * Return: 1 if the number is positive. 0 otherwise
+*/
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
