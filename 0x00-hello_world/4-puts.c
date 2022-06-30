d2aabe12d009b536855c17eac072e440549ca700
robert.mrema <rkarionga@gmail.com>
2022-06-30 20:07:28 +0000

    C commands betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,16 @@
-
 #include<stdio.h>
-
-int main(void) /* main function returns the print statement */
+/**
+ * main - the entry for comment.
+ * void - returns the printed display 
+*/
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-
+/**
+ * puts - prints the text in parenthesis.
+ * @\n: This parameter prints a new line.
+ *
+ * Return: 1 if the number is positive. 0 otherwise
+*/
+	puts("Programming is like building a multilingual puzzle \n ");
 	return (0);
 }
