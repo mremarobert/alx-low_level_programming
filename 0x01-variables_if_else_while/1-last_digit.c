fbe7be63c5313e2fcc3137037bb92bcd0375a601
robert.mrema <rkarionga@gmail.com>
2022-07-01 22:53:23 +0000

    Last digit C- program creation

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 	{
 	printf("Last digit of %d is %d and is zero\n", n, lastdigit);
 	}
-	while (n < 6 && n != 0)
+	else if (n % 10 < 6 && n % 10 != 0)
 	{
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 	}
