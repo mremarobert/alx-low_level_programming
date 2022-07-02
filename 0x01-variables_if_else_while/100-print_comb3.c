b4417c56c9d318fa20eaef0a66d1a7a1ac06ac78
robert.mrema <rkarionga@gmail.com>
2022-07-02 11:39:59 +0000

    printed combination of numbers that are not the same and the first digit is always smaller than the second, implying that the first and second digit dont interchange or appear twice and only the smaller number among the two appears.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,26 +2,24 @@
 /**
  * main - entry point
  * Description: prints combination of two digits with the lowest one preferred.
- * Return - returns success (0)
+ * Return: - return success (0)
  */
 int main(void)
 {
 	int i;
 	int j;
 
-	for(i = 0; i < 10 ; i++)
+	for (i = 0; i < 10 ; i++)
 	{
-	for (j = i+1; j < 10; j++)
+	for (j = i + 1; j < 10; j++)
 	{
 	putchar((i % 10) + '0');
 	putchar((j % 10) + '0');
-
 	if (i == 8 && j == 9)
 	continue;
-	
 	putchar(',');
 	putchar(' ');
-	}	
+	}
 	}
 return (0);
 }
