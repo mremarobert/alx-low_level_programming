82da950e17dd827ab4e7db617e30b34fe3feab17
robert.mrema <rkarionga@gmail.com>
2022-07-02 02:52:15 +0000

    combinations of single digit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints alphabets
+ * Return: returns success
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 48; a <= 57; a++)
+	{
+	putchar(a);
+	{
+	putchar(44);
+	putchar(32);
+	}
+	}
+	putchar('\n');
+return (0);
+}
