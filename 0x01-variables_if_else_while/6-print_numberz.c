3531609fca0d7ef33b82cd1c6a0fd0dcbbeb7fdf
robert.mrema <rkarionga@gmail.com>
2022-07-02 01:35:34 +0000

    print single digit numbers using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: prints alphabets
+ * Return: returns success
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (a = 0; a <= 9; a++)
+	{
+	putchar(a);
+	}
+	putchar('\n');
+return (0);
+}
