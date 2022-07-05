8a357e1b0621ffcd5e0da73b8e8daac75c1de768
robert.mrema <rkarionga@gmail.com>
2022-07-05 23:42:51 +0000

    print every minute of the day of Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,17 +10,17 @@ void jack_bauer(void)
 	m = 0;
 	while (m < 24)
 	{
-	n = 0;
-	while (n < 60)
-	{
-	_putchar((m / 10) + '0');
-	_putchar((m % 10) + '0');
-	_putchar(':');
-	_putchar((n / 10) + '0');
-	_putchar((n % 10) + '0');
-	_putchar('\n');
-	n++
+		n = 0;
+		while (n < 60)
+		{	
+		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
+		_putchar(':');
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+		_putchar('\n');
+		n++;
 	}
-	m++
+	m++;
 	}
 }
