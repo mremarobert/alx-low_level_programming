4545b0e46c144bb865dbce518bfe42d8c252ba13
robert.mrema <rkarionga@gmail.com>
2022-07-05 22:29:18 +0000

    a c function thatp rints the sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 	if (number > 0)
 	{
 		number = 1;
-		putchar(+);
+		putchar('+');
 	}
 	else if (number == 0)
 	{
 		number = 0;
-		putchar('0' + 0);
+		putchar('0');
 	}
 	else
 	{
 		number = -1;
-		putchar(-);
+		putchar('-');
 	{
 	return (number);
 }
