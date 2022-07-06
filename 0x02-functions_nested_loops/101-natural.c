ef6f318307325e9ad968c21c48a488d1443a2074
robert.mrema <rkarionga@gmail.com>
2022-07-06 20:43:39 +0000

    a program that writes the first 98 fibonacci

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ *
+ *
+ */
+int main(void)
+{
+	long int sum3, sum5, sum;
+	int i;
+	
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
+	if((i % 3 ==0)
+	{
+	sum3 = sum3 + i;
+	
+	}else if (
+	for (i = 1; i <10; i++)
+	{
+		m3 = i * 3;
+		m5 = i * 5;
+		sum = m3
+
+		printf("%d\n%d\n", m3, m5);
+		printf("sum: %d\n", sum)
+			
+	}
+
+
+return (0);
+}
