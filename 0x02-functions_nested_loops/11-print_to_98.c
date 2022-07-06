361ef13e177cc568473bbf718e6014577a6c3836
robert.mrema <rkarionga@gmail.com>
2022-07-06 07:05:24 +0000

    prints all natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_to_98 - a function that prints all natural numbers 
+ *
+ *
+ * Return: returns the prints of all natural numbers
+ */
+void print_to_98(int n)
+{
+	for (n = 1; n <= 98; n++)
+	_putchar(n);
+	_putchar(',');
+	_putchar(' ')
+	_putchar('\n');
+}
