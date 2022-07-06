328b2759f0f924fd369d81848ac03620d9273f6a
robert.mrema <rkarionga@gmail.com>
2022-07-06 09:14:21 +0000

    prints the n times table startin with zero

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,10 +13,11 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			for (j == 0; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				k = i * j;
-				if (j == 0;)
+			k = i * j;
+			if (j == 0)
+			{
 				_putchar(k + '0');
 			}
 			else if (k < 10 && j != 0)
@@ -25,7 +26,7 @@ void print_times_table(int n)
 				_putchar(' ');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0')
+				_putchar(k + '0');
 			}
 			else if (k >= 10 && k < 100)
 			{
