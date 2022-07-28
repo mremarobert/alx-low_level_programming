d1963b0ee7eeadea782074c33357ef056428a506
robert.mrema <rkarionga@gmail.com>
2022-07-28 10:10:12 +0000

    a program that adds two positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+
 
 /**
  * main - a program that adds positive numbers
@@ -8,19 +10,32 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc == 1)
+	if (argc < 2)
 	{
-		printf("0");
+		printf("0\n");
 
 	}
 	else
 	{
-		j = 1;
-		for (i = 0; i < argc; i++)
-		j += atoi(argv[i]);
-		printf("%d\n", j);
+		sum = 0;
+		for (i = 1; i < argc; i++)
+		{
+			ptr = argv[i];
+			length = strlen(ptr);
+			for (j = 0; j < length; j++)
+			{
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+		sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
 	}
 	return (0);
 }
