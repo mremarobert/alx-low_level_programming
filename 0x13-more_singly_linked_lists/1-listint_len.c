83be3a46b5da75f4ebc846a689eca55985fd06f0
robert.mrema <rkarionga@gmail.com>
2022-08-16 10:27:36 +0000

    more singly linked

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -9,15 +9,13 @@
  */
 size_t listint_len(const listint_t *h)
 {
-	int count = 0;
+	size_t i = 0;
+	const listint_t *ptr = h;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
+	while (ptr != NULL)
 	{
-		count++;
-		h = h->next;
+		ptr = ptr->next;
+		i++;
 	}
-	return (count);
+	return (i);
 }
