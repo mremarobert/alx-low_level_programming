4cece5220a241ab1c1df91f9895be4b22250d705
robert.mrema <rkarionga@gmail.com>
2022-08-16 10:40:25 +0000

    more singly linked

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,13 +3,13 @@
 /**
  * free_listint - frees a linked list
  * @head: head of a list
- * 
+ *
  * Return: no return.
  */
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
-	
+
 	while ((temp = head) != NULL)
 	{
 		head = head->next;
