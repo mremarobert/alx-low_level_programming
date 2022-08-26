c386bc15a822bdbecd7efcbdba0785e444352f7e
robert.mrema <rkarionga@gmail.com>
2022-08-26 23:17:51 +0000

    file descriptors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,28 +7,41 @@
  *
  * Return: numbers of letters printed. it fails, return 0.
  */
-ssize_t read_textfile(const char *filename, sie_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t nrd, nwr;
+	int file, i = 0;
 	char *buf;
+	ssize_t n;
 
-	if (!filename)
+	if (!filename || !letters)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
 
-	if (fd == -1)
+	file = open(filename, O_RDONLY);
+	if (file < 0)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
+
+	buf = malloc(sizeof(char) * letters);
 	if (!buf)
 		return (0);
 
-	nrd = read(fd, buf, letters);
-	nwr = write(STDOUT_FILENO, buf, nrd);
 
-	close(fd);
+	n = read(file, buf, letters);
+	if (n < 0)
+	{
+		free(buf);
+		return (0);
+	}
+
+
+	while (buf && buf[i])
+		i++;
+	n = write(STDOUT_FILENO, buf, i);
 	free(buf);
-	return (nwr);
+	close(file);
+	if (n < 0)
+		return (0);
+
+	return (n);
 }
