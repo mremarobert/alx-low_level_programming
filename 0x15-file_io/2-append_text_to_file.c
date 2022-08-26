11d12ef759d33f583c5e1e8874cb6cccb5a7e93d
robert.mrema <rkarionga@gmail.com>
2022-08-26 23:23:33 +0000

    file descriptors

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,26 +9,33 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
-	int nletters;
-	int rwr;
+	int file, wr, i = 0;
 
-	if (filename)
-		return (-1);
-	fd = open(filename, O_WRONLY |	O_APPEND);
-	if (fd == -1)
+	if (filename == NULL)
 		return (-1);
 
-	if (text_content)
+	file = open(filename, O_RDWR | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+
+
+	while (text_content[i])
+		i++;
+
+	if (text_content == NULL)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-		;
-
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
-		return (-1);
+		close(file);
+		return (1);
 	}
-	close(fd);
+	else
+	{
+		wr = write(file, text_content, i);
+	}
+
+	if (wr == -1)
+		return (-1);
+
+	close(file);
 	return (1);
 }
