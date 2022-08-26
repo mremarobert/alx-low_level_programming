687c6656bdd9dbd1b41c7f046089b450e44244cf
robert.mrema <rkarionga@gmail.com>
2022-08-26 23:38:20 +0000

    file descriptors

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,5 +12,9 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int cp(int argc, char *argv[]);
+int tests(int argc, char **argv, char *buf, int f1);
+int tests2(char **argv, char *buf, int r, int f1, int f2, int wr);
+
 
 #endif
